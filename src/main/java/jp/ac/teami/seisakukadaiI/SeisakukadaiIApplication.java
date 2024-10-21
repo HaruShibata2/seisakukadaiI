bbde39dd2519776eea10f29357c82f59b373296a
HaruShibata3 <haruhiro22329@gmail.com>
2024-10-21 23:55:38 +0900

    tukareta

---
+++ src/main/java/jp/ac/teami/seisakukadaiI/SeisakukadaiIApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.teami.seisakukadaiI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeisakukadaiIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeisakukadaiIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/teami/seisakukadaiI/SeisakukadaiIApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.teami.seisakukadaiI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeisakukadaiIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
