e803fcb6ad92d951d4eaa3f908fe4d5c5b2aecd8
TatsuyaTakakusa <fko2347056@stu.o-hara.ac.jp>
2024-10-24 12:02:12 +0900

    add

--- src/main/java/jp/ac/teami/seisakukadaiI/controller/LoginController.java
+++ src/main/java/jp/ac/teami/seisakukadaiI/controller/LoginController.java
@@ -1,3 +1,4 @@
+
 package jp.ac.teami.seisakukadaiI.controller;
 
 import org.springframework.stereotype.Controller;
@@ -6,20 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-
-    //GETメソッド
-    @GetMapping("/login/syokigamen")
-    public String getLogin(Model model) {
-        
-        return "login/syokigamen";
-    }
-
-    @GetMapping("/login/kojintouroku")
-    public String index(Model model2) {
-    	return "login/kojintouroku";
-    }
-    
-
-    
+	
+	//GETメソッド
+	@GetMapping("/login/syokigamen")
+	public String getLogin(Model model) {
+		
+		return "login/syokigamen";
+	}
+	
+	@GetMapping("/login/kojintouroku")
+	public String index(Model model) {
+		return "login/kojintouroku";
+	}
+	
+	@GetMapping("/login/login")
+	public String index2(Model model) {
+		return "login/login";
+	}
+	
+	
+	
 }
-
