b8732d5147a595b92459d0a7232c403e061404d3
HaruShibata3 <haruhiro22329@gmail.com>
2025-02-10 03:49:06 +0900

    update_atの追加

--- src/main/resources/db/migration/V6__SafetyChecks_table.sql
+++ src/main/resources/db/migration/V6__SafetyChecks_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE `Safetychecks` (
     `username` VARCHAR(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'ユーザー名（外部キー）',
     `status` ENUM('安全', '怪我') NOT NULL COMMENT '安否状態',
     `checked_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '安否確認日時',
+    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'レコード更新日時',
     PRIMARY KEY (`safe_id`),
     FOREIGN KEY (`username`) REFERENCES `Users`(`username`) ON DELETE CASCADE
 ) ENGINE=InnoDB;
